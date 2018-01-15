fc2dc39374afaca826ddc42913bbd931f92ba1f4
Zac Littleberry <zaclittleberry@gmail.com>
2018-01-14 23:57:56 -0500

    add gitignore and update install node

--- install_node.sh
+++ install_node.sh
@@ -2,6 +2,7 @@
 
 function install_node {
   sudo apt-get install nodejs -y
+  sudo ln -s /usr/bin/nodejs /usr/bin/node
   sudo apt-get install npm -y
   mkdir ~/.npm-global
   npm config set prefix '~/.npm-global'
