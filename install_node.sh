d2b3258e3d0d1d3425148ae80690daff80b38aa8
Zac Littleberry <zaclittleberry@gmail.com>
2018-01-14 22:46:08 -0500

    first commit

---
+++ install_node.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function install_node {
+  sudo apt-get install nodejs -y
+  sudo apt-get install npm -y
+  mkdir ~/.npm-global
+  npm config set prefix '~/.npm-global'
+  echo "if [ -d \"$HOME/.npm-global/bin\" ] ; then" >> ~/.profile
+  echo "  PATH=\"$HOME/.npm-global/bin:$PATH\"" >> ~/.profile
+  echo "fi" >> ~/.profile
+}
+
+install_node
