67ba9094e937bb3085824e57f04835394627e811
Zac Littleberry <zaclittleberry@gmail.com>
2018-02-24 00:09:14 -0500

    add body parser. add key tap. fix mouse down and right

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const robot = require("robotjs");
 
 // Speed up the mouse.
 robot.setMouseDelay(2);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.set('json spaces', 2);
 
@@ -21,30 +31,37 @@ app.get('/mouse', (req, res) => {
 });
 
 app.post('/up/:amount', (req, res) => {
-  let amount = req.params.amount;
+  let amount = parseInt(req.params.amount);
   var mouse = getMousePos();
-  robot.moveMouse(mouse.x, mouse.y - amount);
+  robot.moveMouse(mouse.x, (mouse.y - amount));
   res.json({status:'OK'});
 });
 
 app.post('/down/:amount', (req, res) => {
-  let amount = req.params.amount;
+  let amount = parseInt(req.params.amount);
   var mouse = getMousePos();
-  robot.moveMouse(mouse.x, mouse.y + amount);
+  robot.moveMouse(mouse.x, (mouse.y + amount));
   res.json({status:'OK'});
 });
 
 app.post('/left/:amount', (req, res) => {
-  let amount = req.params.amount;
+  let amount = parseInt(req.params.amount);
   var mouse = getMousePos();
-  robot.moveMouse(mouse.x - amount, mouse.y);
+  robot.moveMouse((mouse.x - amount), mouse.y);
   res.json({status:'OK'});
 });
 
 app.post('/right/:amount', (req, res) => {
-  let amount = req.params.amount;
+  let amount = parseInt(req.params.amount);
   var mouse = getMousePos();
-  robot.moveMouse(mouse.x + amount, mouse.y);
+  robot.moveMouse((mouse.x + amount), mouse.y);
+  res.json({status:'OK'});
+});
+
+app.post('/keyTap', (req, res) => {
+  let key = req.body.key;
+  let modifier = req.body.modifier;
+  robot.keyTap(key, modifier);
   res.json({status:'OK'});
 });
 
